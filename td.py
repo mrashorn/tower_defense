55414989e876ad5da777d79fc384bbcff19e3bd2
Matt Ashorn <matthew.ashorn@utexas.edu>
2021-09-14 06:50:54 -0500

    Added the bare bones for displaying a window, created the main game loop. This is just the beginning.

--- td.py
+++ td.py
@@ -1,1 +1,41 @@
-# first commit test
+import sys
+import pygame
+
+class TowerDefense:
+    """Overall class to manage assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Tower Defense")
+
+        self.bg_color = (0, 0, 0)
+
+
+    def run_game(self):
+        """Start the main game loop."""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+
+    def _check_events(self):
+        """Respond to keyboard and mouse presses."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    
+    def _update_screen(self):
+        """Update images on screen, flip to new screen."""
+        self.screen.fill(self.bg_color)
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance and run the game.
+    td = TowerDefense()
+    td.run_game()
