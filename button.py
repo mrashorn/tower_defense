2cd3bdde969af73117f9c57a98c746a13c83cc77
Matt Ashorn <matthew.ashorn@utexas.edu>
2021-09-17 10:47:19 -0500

    Display the map to background screen, made screen larger for menu items. Added non-functioning button for Build Tower. To use to build towers during programming.

---
+++ button.py
@@ -0,0 +1,35 @@
+import pygame.font
+
+class Button:
+    """Class to handle any selections the player wants to make during the game."""
+
+    def __init__(self, td_game, msg):
+        """Initialize button attributes."""
+        self.screen = td_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button
+        self.width, self.height = 200, 50
+        self.button_color = (25, 25, 25)
+        self.text_color = (200, 200, 200)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center the rect
+        self.rect = pygame.Rect(0, 0, self.width, self.height) # (left, top, width, height)
+        self.rect.center = (650, 850) # this is temporary for the first button Build Tower
+
+        # The button message needs to be prepped once.
+        self._prep_msg(msg)
+
+
+    def _prep_msg(self, msg):
+        """Turn the message into a rendered image and center the text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+
+    def draw_button(self):
+        """Draw a blank button then draw the message."""
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
--- td.py
+++ td.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from button import Button
 
 class TowerDefense:
     """Overall class to manage assets and behavior"""
@@ -15,6 +16,14 @@ class TowerDefense:
         self.screen = pygame.display.set_mode((
             self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Tower Defense")
+
+        # Set up the map
+        self.map_image = pygame.image.load('images/map.bmp')
+        self.map_rect = self.map_image.get_rect()
+
+        # Create the first build tower button
+        self.build_tower_button = Button(self, "Build Tower")
+
 
 
 
@@ -34,7 +43,11 @@ class TowerDefense:
     
     def _update_screen(self):
         """Update images on screen, flip to new screen."""
-        self.screen.fill(self.settings.bg_color)
+        self.screen.fill(self.settings.bg_color) # can remove once the map is done
+        # Draw the map
+        self.screen.blit(self.map_image, self.map_rect)
+        self.build_tower_button.draw_button() # eventually probably have a function that draws all buttons.
+
 
         pygame.display.flip()
 
